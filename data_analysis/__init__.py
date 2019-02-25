6b2595f75eb92d0504cdded2c0a5daa9bd67bfa2
chenmin <350376345@qq.com>
2019-02-25 10:13:28 +0800

    异常和日志

---
+++ data_analysis/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# author:35037
+# datetime:2019/2/25 9:57
---
+++ spider/example-1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# author:35037
+# datetime:2019/2/25 9:54
+
+'''
+爬虫的实质是：本地从服务器上抓取数据的过程。
+'''
