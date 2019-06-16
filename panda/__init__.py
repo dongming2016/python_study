f14bdf79c6d826ab61a4aa9a991e08be9109c65e
chenmin <350376345@qq.com>
2019-06-16 23:10:43 +0800

    删除无用分支

--- panda/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# author:35037
-# datetime:2019/2/13 21:46
--- panda/read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# author:35037
-# datetime:2019/2/13 21:47
-
-import panda as pd
-
-pd.read_csv('./Haiti.csv')
